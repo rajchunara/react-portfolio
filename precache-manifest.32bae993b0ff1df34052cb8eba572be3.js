10e5494038dc16ef264041f758ef8dc6b6f11daf
rajchunara <rajchunara777@gmail.com>
2020-05-10 12:22:31 -0400

    Updates

---
+++ precache-manifest.32bae993b0ff1df34052cb8eba572be3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "197de2ff5328004cb0e4d3b5737856b2",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "17dd9f8d73c4b12ab043",
+    "url": "/react-portfolio/static/css/main.b87a783b.chunk.css"
+  },
+  {
+    "revision": "4d3dfdfc8fdb9259d773",
+    "url": "/react-portfolio/static/js/2.5f064bb4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.5f064bb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17dd9f8d73c4b12ab043",
+    "url": "/react-portfolio/static/js/main.2319c3ed.chunk.js"
+  },
+  {
+    "revision": "9cebcfe87e74c077e3cf",
+    "url": "/react-portfolio/static/js/runtime-main.d0dd671a.js"
+  }
+]);
