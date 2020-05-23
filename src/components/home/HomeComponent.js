ee434a08092db167bad1d48caff670e450e75368
rajchunara <rajchunara777@gmail.com>
2020-05-23 19:29:25 -0400

    small change in typewriter effect

--- src/components/home/HomeComponent.js
+++ src/components/home/HomeComponent.js
@@ -10,15 +10,29 @@ export default function HomeComponent(props) {
             
             <div className='showcase' >
                 <div className='container showcaseInner fade-in' >
-                    <h1>Hello, I am Raj Chunara</h1>
-                    <h1>
-                        <Typewriter
-                            options={{
-                                strings: ['Developer', 'Designer', 'Creator'],
-                                autoStart: true,
-                                loop: true,
-                            }}
-                        />
+                    <h1>Hello, I am Raj </h1>
+                    <h1>    
+                    <Typewriter
+
+                        onInit={(typewriter) => {
+                            typewriter.typeString('-Developer')
+                            .pauseFor(1500)
+                            .deleteChars(9)
+                            typewriter.typeString('Designer')
+                            .pauseFor(1500)
+                            .deleteChars(8)
+                            typewriter.typeString('Creator')
+                            .pauseFor(1500)
+                            .deleteChars(7)
+                            .start();
+                        }}
+
+                        options={{
+                            autoStart:true,
+                            loop:true
+                        }}
+                    />                
+                    
                     </h1>
                     <p className="description">
                         I am Toronto based software developer, who likes creating beautiful 
